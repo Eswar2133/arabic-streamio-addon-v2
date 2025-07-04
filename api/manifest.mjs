53de5ceddb1bf9a34ae3ebd4e52bceff737d5eec
Eswar2133 <kuwait2412@gmail.com>
2025-07-04 13:04:48 +0300

    Update manifest.mjs

--- api/manifest.mjs
+++ api/manifest.mjs
@@ -4,24 +4,24 @@ export default function handler(req, res) {
     id: "org.arabic.archive",
     version: "1.0.0",
     name: "Arabic Classics",
-    description: "Arabic-language movies from the Internet Archive. ✅ Legal & universal. ⚡ For best quality playback, use Real-Debrid.",
+    description: "Arabic-language movies from the Internet Archive. ✅ Legal & universal.",
     logo: "https://upload.wikimedia.org/wikipedia/commons/8/87/Internet_Archive_logo_and_wordmark.svg",
-    resources: [
-      {
-        name: "stream",
-        types: ["movie"]
-      },
-      {
-        name: "catalog",
-        types: ["movie"]
-      }
-    ],
     types: ["movie"],
     catalogs: [
       {
         type: "movie",
         id: "arabic-classics",
         name: "Arabic Archive"
+      }
+    ],
+    resources: [
+      {
+        name: "catalog",
+        types: ["movie"]
+      },
+      {
+        name: "stream",
+        types: ["movie"]
       }
     ]
   });
